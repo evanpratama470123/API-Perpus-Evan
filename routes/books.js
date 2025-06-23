c7269ed2be9d09632bf2f715e7de580d3a491ab3
Evan Pratama <pratamaevan2005@gmail.com>
2025-06-23 15:17:11 +0700

    LUPA SLASH DOANG

--- routes/books.js
+++ routes/books.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Book = require('../models/book');
-const db = require('..config/db');
+const db = require('../config/db');
 
 // GET semua buku
 router.get('/semua', (req, res) => {
