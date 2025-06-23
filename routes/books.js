8989744f16290be45d66965eb45846b031dd0368
Evan Pratama <pratamaevan2005@gmail.com>
2025-06-23 15:15:37 +0700

    forgot the config folder

--- routes/books.js
+++ routes/books.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Book = require('../models/book');
-const db = require('../db');
+const db = require('..config/db');
 
 // GET semua buku
 router.get('/semua', (req, res) => {
